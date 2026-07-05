47e26987d903926e0d2536731f789adb51826b6b
ooestvo46-design <ooestvo46@gmail.com>
2026-07-05 22:33:01 +0200

    Update frame parameters

--- config.py
+++ config.py
@@ -4,45 +4,26 @@ Configuration
 """
 
 PARAMETERS = {
-
-    # -----------------------------
-    # Cans
-    # -----------------------------
-
     "canDiameter": 67.0,
     "canHeight": 168.0,
-
-    # -----------------------------
-    # Ice pack
-    # -----------------------------
 
     "iceWidth": 80.0,
     "iceThickness": 20.0,
     "iceHeight": 150.0,
 
-    # -----------------------------
-    # Structure
-    # -----------------------------
-
     "wall": 3.0,
     "bottomThickness": 4.0,
     "guideRail": 2.0,
+    "guideRailHeight": 55.0,
     "airGap": 4.0,
-
-    # -----------------------------
-    # Styling
-    # -----------------------------
 
     "fillet": 5.0,
     "textHeight": 0.8,
     "textDepth": 0.8,
 
-    # -----------------------------
-    # General
-    # -----------------------------
-
     "canCount": 6
 }
 
 
+# Fusion API uses centimeters internally.
 MM_TO_CM = 0.1
